81014c9d238faac6b5344ac9292a16c5f6f2208d
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-06 12:01:13 -0400

    addd the initial stuff to get the project working

---
+++ gpu-subsurf.cpp
@@ -0,0 +1,9 @@
+// trying to get the catmull clark subdiv method working in c++ so I can translate it to cuda
+
+#include <iostream>
+#include <math.h>
+#include <string>
+
+int main (void) {
+    return 0;
+}
