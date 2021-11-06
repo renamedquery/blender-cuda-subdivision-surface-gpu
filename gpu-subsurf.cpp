6d74461921ef5187d678f8dce294ca18586d3fe6
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-06 13:13:11 -0400

    readObj can now read verts, texuture coordinates, and normals

--- gpu-subsurf.cpp
+++ gpu-subsurf.cpp
@@ -6,14 +6,55 @@
 #include <fstream>
 #include <iterator>
 #include <sstream>
+#include <vector>
 
 using namespace std;
+
+struct vec3 {
+    double x;
+    double y;
+    double z;
+};
+
+struct vec2 {
+    double x;
+    double y;
+};
+
+struct vertex {
+    vec3 position;
+    vec2 textureCoordinate;
+    vec3 normal;
+};
+
+std::vector<std::string> stringSplit(std::string string, char delimiter) {
+
+    std::vector<std::string> splitString;
+    std::string currentString = "";
+
+    for (int i = 0; i < string.length(); i++) {
+        if (string[i] == delimiter) {
+
+            splitString.push_back(currentString);
+            currentString = "";
+        } else {
+
+            currentString += string[i];
+
+            if (i + 1 == string.length()) {
+                splitString.push_back(currentString);
+            }
+        }
+    }
+
+    return splitString;
+}
 
 // can be gpu accelerated at least a little bit since it requires many lines to be read
 // for a simple cube there will be no speedup, but for a mesh with millions of verts it will be faster
 // currently only reads verts, faces and edges are todo
 // __global__
-void readObj(std::string path, std::string& output) {
+void readObj(std::string path, std::vector<vertex> vertices) {
     
     std::ifstream objFile(path);
 
@@ -28,27 +69,28 @@ void readObj(std::string path, std::string& output) {
         char objFileLineChar;
         ss >> objFileLineChar;
 
-        switch (objFileLineChar) {
+        std::vector<std::string> lineDataSplitBySpaces = stringSplit(objFileLine, ' ');
+        std::string lineType = lineDataSplitBySpaces[0];
 
-            case 'v': { // vert
+        vertex currentVert;
 
-                char objFileLineSecondChar = objFileLine[1];
-                
-                switch (objFileLineSecondChar) {
+        cout << objFileLine << endl;
 
-                    case ' ': { // empty second line, meaning it isnt vt/vn and is just a vert
+        if (lineType.compare("v") == 0) {
+            currentVert.position.x = std::stod(lineDataSplitBySpaces[1]);
+            currentVert.position.y = std::stod(lineDataSplitBySpaces[2]);
+            currentVert.position.z = std::stod(lineDataSplitBySpaces[3]);
 
-                        output += objFileLine + "\n";
-                        break;
-                    }
-                }
-                break;
-            }
+        } else if (lineType.compare("vt") == 0) {
+            currentVert.textureCoordinate.x = std::stod(lineDataSplitBySpaces[1]);
+            currentVert.textureCoordinate.x = std::stod(lineDataSplitBySpaces[2]);
 
-            case 'f': { // face
+        } else if (lineType.compare("vn") == 0) {
+            currentVert.normal.x = std::stod(lineDataSplitBySpaces[1]);
+            currentVert.normal.y = std::stod(lineDataSplitBySpaces[2]);
+            currentVert.normal.z = std::stod(lineDataSplitBySpaces[3]);
 
-                break;
-            }
+        } else {
         }
     }
 }
@@ -56,11 +98,9 @@ void readObj(std::string path, std::string& output) {
 int main (void) {
 
     std::string objPath = "./testCube.obj";
-    std::string objData = "";
+    std::vector<vertex> objVertices;
 
-    readObj(objPath, objData);
-
-    cout << objData << endl;
+    readObj(objPath, objVertices);
 
     return 0;
 }
