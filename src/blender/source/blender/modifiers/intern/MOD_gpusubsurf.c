2ba0fdc86a05504b34780a64f43fbaeb02622065
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-14 19:49:40 -0500

    fixed the errors with MOD_gpusubsurf.c

--- src/blender/source/blender/modifiers/intern/MOD_gpusubsurf.c
+++ src/blender/source/blender/modifiers/intern/MOD_gpusubsurf.c
@@ -1,24 +1,48 @@
 // adapted from the instructions on https://blog.exppad.com/article/writing-blender-modifier
 
-#include "BKE_modifier.h"
-#include "BKE_mesh.h"
+// created by katznboyz, but adapted from the instructions above, and integrated into code by the Blender Foundation
 
+/** \file
+ * \ingroup modifiers
+ */
+
+#include "DNA_defaults.h"
 #include "DNA_mesh_types.h"
+#include "DNA_meshdata_types.h"
+#include "DNA_object_types.h"
+#include "DNA_screen_types.h"
+
+#include "BKE_action.h"
+#include "BKE_colortools.h"
+#include "BKE_context.h"
+#include "BKE_deform.h"
+#include "BKE_editmesh.h"
+#include "BKE_lib_id.h"
+#include "BKE_lib_query.h"
+#include "BKE_mesh.h"
+#include "BKE_mesh_wrapper.h"
+#include "BKE_modifier.h"
+#include "BKE_screen.h"
+
+#include "RNA_access.h"
 
 ModifierTypeInfo modifierType_GPUSubsurf = {
     /* name */ "GPU Subdivision Surface",
     /* structName */ "GPUSubsurfData",
     /* structSize */ sizeof(GPUSubsurfData),
-    /* type */ eModifierTypeType_None,
-    /* flags */ eModifierTypeFlag_AcceptsMesh,
-
+    /* srna */ NULL,
+    /* type */ eModifierType_GPUSubsurf,
+    /* flags */ eModifierTypeType_None,
+    /* icon */ NULL,
     /* copyData */ NULL,
 
-    /* deformVerts */ true,
-    /* deformMatrices */ true,
-    /* deformVertsEM */ true,
-    /* deformMatricesEM */ true,
-    /* applyModifier */ true,
+    /* deformVerts */ NULL,
+    /* deformMatrices */ NULL,
+    /* deformVertsEM */ NULL,
+    /* deformMatricesEM */ NULL,
+    /* modifyMesh */ NULL,
+    /* modifyHair */ NULL,
+    /* modifyGeometrySet */ NULL,
 
     /* initData */ NULL,
     /* requiredDataMask */ NULL,
@@ -27,11 +51,14 @@ ModifierTypeInfo modifierType_GPUSubsurf = {
     /* updateDepsgraph */ NULL,
     /* dependsOnTime */ NULL,
     /* dependsOnNormals */ NULL,
-    /* foreachObjectLink */ NULL,
     /* foreachIDLink */ NULL,
     /* foreachTexLink */ NULL,
     /* freeRuntimeData */ NULL,
+    /* panelRegister */ NULL,
+    /* blendWrite */ NULL,
+    /* blendRead */ NULL,
 };
+
 
 static Mesh *gpusubsurf_applyModifier(struct ModifierData *md, const struct ModifierEvalContext *ctx, struct Mesh *mesh) {
     
