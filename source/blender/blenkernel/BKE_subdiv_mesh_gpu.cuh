f618f6cb404cc13bbc9a7193961b7fdda8d4b13a
katznboyz1 <harrison.lindgren@gmail.com>
2021-11-19 20:02:50 -0500

    reduced the amount of compilation errors

--- source/blender/blenkernel/BKE_subdiv_mesh_gpu.cuh
+++ source/blender/blenkernel/BKE_subdiv_mesh_gpu.cuh
@@ -32,6 +32,18 @@ extern "C" {
 struct Mesh;
 struct Subdiv;
 
+typedef struct SubdivToMeshSettings {
+  /* Resolution at which regular ptex (created for quad polygon) are being
+   * evaluated. This defines how many vertices final mesh will have: every
+   * regular ptex has resolution^2 vertices. Special (irregular, or ptex
+   * created for a corner of non-quad polygon) will have resolution of
+   * `resolution - 1`.
+   */
+  int resolution;
+  /* When true, only edges emitted from coarse ones will be displayed. */
+  bool use_optimal_display;
+} SubdivToMeshSettings;
+
 /* Create real hi-res mesh from subdivision, all geometry is "real". */
 struct Mesh *BKE_subdiv_to_mesh_cuda(struct Subdiv *subdiv,
                                 const SubdivToMeshSettings *settings,
